b0b4e40a610fd593d077b6d2978c17e9dea51a55
bill roca <bill.egoavil.m@gmail.com>
2024-07-31 21:51:09 -0500

    reemplazo comilla doble

--- src/main_pub_cb.cpp
+++ src/main_pub_cb.cpp
@@ -2,17 +2,17 @@
 
 bool PUB_Sensor_app_cb(char *buffer, int *size)
 {
-  int bytes = snprintf(buffer, *size, "\"{"
-                                      "'dis':[%i,%i,%i,%i,%i,%i,%i,%i,%i],"
-                                      "'vel':[%i,%i,%i,%i,%i],"
-                                      "'inc':%i,"
-                                      "'ori':%i,"
-                                      "'tem':%i,"
-                                      "'hum':%i,"
-                                      "'uls':%i,"
-                                      "'lls':%i,"
-                                      "'vmx':%i"
-                                      "}\"",
+  int bytes = snprintf(buffer, *size, "{"
+                                      "\"dis\":[%i,%i,%i,%i,%i,%i,%i,%i,%i],"
+                                      "\"vel\":[%i,%i,%i,%i,%i],"
+                                      "\"inc\":%i,"
+                                      "\"ori\":%i,"
+                                      "\"tem\":%i,"
+                                      "\"hum\":%i,"
+                                      "\"uls\":%i,"
+                                      "\"lls\":%i,"
+                                      "\"vmx\":%i"
+                                      "}",
                        distance_get_Front_mm() / 10, distance_get_RightFront_mm() / 10,
                        distance_get_RightMiddle_mm() / 10, distance_get_RightBack_mm() / 10,
                        distance_get_LeftFront_mm() / 10, distance_get_LeftMiddle_mm() / 10,
@@ -36,15 +36,15 @@ bool PUB_Sensor_app_cb(char *buffer, int *size)
 bool PUB_AlertSauron_app_cb(char *buffer, int *size)
 {
 
-  int bytes = snprintf(buffer, *size, "\"{"
-                                      "'dis':[%i,%i,%i,%i,%i,%i,%i,%i,%i],"
-                                      "'mot':[%i,%i,%i,%i,%i],"
-                                      "'inc':%i,"
-                                      "'tem':%i,"
-                                      "'hum':%i,"
-                                      "'uls':%i,"
-                                      "'lls':%i"
-                                      "}\"",
+  int bytes = snprintf(buffer, *size, "{"
+                                      "\"dis\":[%i,%i,%i,%i,%i,%i,%i,%i,%i],"
+                                      "\"mot\":[%i,%i,%i,%i,%i],"
+                                      "\"inc\":%i,"
+                                      "\"tem\":%i,"
+                                      "\"hum\":%i,"
+                                      "\"uls\":%i,"
+                                      "\"lls\":%i"
+                                      "}",
                        1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1);
   bool parsing_success = (bytes > 0) || (bytes < (*size));
   *size = bytes + 1;
